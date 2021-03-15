685d0c558a5bc99d1bafc71c028348b8742d5378
Martin-Prusa <73097610+Martin-Prusa@users.noreply.github.com>
2021-03-15 09:33:18 +0100

    css

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-let isBlack = true;
-
 document.write('<div class="container">')
 
 for (let i = 0; i < 8; i++) {
     document.write('<div class="radek">')
 
     for (let j = 0; j < 8; j++) {
-        document.write(`<div class="pole ${isBlack ? 'black' : ''}"></div>`)
-        isBlack = !isBlack
+        document.write(`<div class="pole"></div>`)
     }
-    isBlack = !isBlack
     document.write('</div>')
 }
 
